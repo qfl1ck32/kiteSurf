94183330b27d2425b30891cc45ca5b6c398f8c70
qfl1ck32 <qfl1ck32@yahoo.com>
2021-03-25 21:23:28 +0200

    Design login page

---
+++ src/components/FormField.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { MDBContainer } from 'mdbreact'
+import { Form } from 'react-bootstrap'
+
+import '../assets/css/formField.css'
+
+const FormField = (props) => (
+    <Form.Group className = 'text-left'>
+        <Form.Label className = 'text-white'>{ props.name + ':' }</Form.Label>
+
+        <Form.Control 
+            autoComplete = 'off'
+            required
+            type = { props.type }
+            name = { props.fieldName }
+            isValid = { !props.errors[props.fieldName] && (props.values[props.fieldName] || props.touched[props.fieldName]) }
+            isInvalid = { props.errors[props.fieldName] && (props.values[props.fieldName] || props.touched[props.fieldName]) }
+            onChange = { props.handleChange }
+            value = { props.values[props.fieldName] }
+            placeholder = { props.placeHolder }
+            className = 'mb-1 rounded' />
+
+        <MDBContainer className = { 'text-left alert alert-danger rounded ' + (props.errors[props.fieldName] && (props.values[props.fieldName] || props.touched[props.fieldName]) ? (props.values.fadeErrors ? 'fadeIn' : 'hidden') : 'd-none') }>
+        
+            { props.errors[props.fieldName] && (props.values[props.fieldName] || props.touched[props.fieldName]) ? props.errors[props.fieldName] : '' }
+        
+        </MDBContainer>
+
+    </Form.Group>
+)
+
+export default function NewField(props) {
+    console.log('creat')
+    return (
+        <FormField 
+            name = { props.name }
+            fieldName = { props.fieldName }
+            errors = { props.errors }
+            values = { props.values }
+            touched = { props.touched }
+            handleChange = { props.handleChange }
+            placeHolder = { props.placeHolder } 
+            type = { props.type } />
+    )
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,20 +5,82 @@ import '../assets/css/login.css'
 import Kite from '../assets/images/Kite.svg'
 
 import { MDBContainer, MDBJumbotron } from 'mdbreact'
-import { Form, Image } from 'react-bootstrap'
+import { Form, Image, Button } from 'react-bootstrap'
+
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'mdbreact/dist/css/mdb.css'
 
+import NewField from '../components/FormField'
+
 function Login() {
+
+    const validationSchema = Yup.object({
+        username: Yup.string()
+            .required('This field should not be empty.')
+            .min(2, 'Username should have a length of at least two characters.'),
+        password: Yup.string()
+            .required('This field should not be empty.')
+            .min(6, 'Password should be at least 6 characters long.')
+    })
+
+    const { errors, touched, values, handleChange, handleSubmit, setValues } = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+
+            fadeErrors: true
+        },
+
+        onSubmit: async () => {
+            // login
+        },
+
+        validationSchema
+    })
+
+    const handleSubmitFadeErrors = e => {
+        handleSubmit(e)
+
+        setValues({...values, ...{'fadeErrors': false}})
+
+        setTimeout(() => {
+            setValues({...values, ...{'fadeErrors': true}})
+        }, 0)
+    }
+
+    const newFieldSetup = (name, fieldName, placeholder, type) => {
+        return {
+            name: name,
+            fieldName: fieldName,
+            errors: errors,
+            touched: touched,
+            values: values,
+            placeHolder: placeholder,
+            type: type,
+            handleChange: handleChange
+          }
+    }
+
+    const username = NewField(newFieldSetup('Username', 'username', 'George', 'text'))
+    const password = NewField(newFieldSetup('Password', 'password', '••••••••', 'password'))
 
     return (
         <MDBContainer fluid className = 'main d-flex align-items-center justify-content-center'>
-            <MDBJumbotron className = 'w-50 bg-primary text-dark'>
+            <MDBJumbotron className = 'w-50 bg-primary'>
 
                 <MDBContainer className = 'display-4 text-center'>
                     <Image fluid src = { Kite } />
                 </MDBContainer>
+
+                <Form className = 'text-center' noValidate onSubmit = { handleSubmitFadeErrors }>
+                    { username }
+                    { password }
+
+                    <Button className = 'text-white bg-primary' type = 'submit'>Login</Button> 
+                </Form>
 
             </MDBJumbotron>
         </MDBContainer>
